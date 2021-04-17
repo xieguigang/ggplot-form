6071797780718ba6dab4db84bf2ffe37266033e9
xieguigang <xie.guigang@gcmodeller.org>
2021-04-18 01:05:29 +0800

    framework for show plot

--- PlotKit/PlotView.vb
+++ PlotKit/PlotView.vb
@@ -1,5 +1,25 @@
-﻿Imports System.Windows.Forms
+﻿Imports System.Drawing
+Imports System.Windows.Forms
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports PlotPadding = Microsoft.VisualBasic.MIME.Markup.HTML.CSS.Padding
 
 Public Class PlotView : Inherits Control
 
+    Public Property Chart As Plot
+    Public Property PlotPadding As PlotPadding
+
+    Private Sub PlotView_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Not Chart Is Nothing Then
+            Dim g As Graphics = Me.CreateGraphics
+            Dim canvas As New Graphics2D(g, Me.Size)
+            Dim region As New GraphicsRegion With {
+                .Padding = Me.PlotPadding,
+                .Size = Me.Size
+            }
+
+            Call Chart.Plot(canvas, region)
+        End If
+    End Sub
 End Class
