d3cca40ca453eb2ed5eec06864df6c0aa12f445f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-19 19:01:25 +0800

    do rendering view of ggplot

--- PlotKit/PlotView.vb
+++ PlotKit/PlotView.vb
@@ -1,38 +1,37 @@
-﻿Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic
-Imports Microsoft.VisualBasic.Imaging
+﻿Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
 Imports PlotPadding = Microsoft.VisualBasic.MIME.Html.CSS.Padding
 
 Public Class PlotView
 
-    'Dim _chart As Plot
+    Dim m_ggplot As ggplot.ggplot
 
-    'Public Property Chart As Plot
-    '    Get
-    '        Return _chart
-    '    End Get
-    '    Set(value As Plot)
-    '        _chart = value
-    '        Rendering()
-    '    End Set
-    'End Property
+    Public Property ggplot As ggplot.ggplot
+        Get
+            Return m_ggplot
+        End Get
+        Set
+            m_ggplot = Value
+            Rendering()
+        End Set
+    End Property
 
-    'Public Property PlotPadding As PlotPadding = g.DefaultPadding
+    Public Property PlotPadding As PlotPadding = g.DefaultPadding
 
-    'Private Sub Rendering()
-    '    If Not Chart Is Nothing Then
-    '        Dim g As Graphics = Me.CreateGraphics
-    '        Dim canvas As New Graphics2D(g, Me.Size)
-    '        Dim region As New GraphicsRegion With {
-    '            .Padding = Me.PlotPadding,
-    '            .Size = Me.Size
-    '        }
+    Private Sub Rendering()
+        If Not ggplot Is Nothing Then
+            Dim g As Graphics = Me.CreateGraphics
+            Dim canvas As New Graphics2D(g, Me.Size)
+            Dim region As New GraphicsRegion With {
+                .Padding = Me.PlotPadding,
+                .Size = Me.Size
+            }
 
-    '        Call Chart.Plot(canvas, region)
-    '    End If
-    'End Sub
+            Call ggplot.Plot(canvas, region)
+        End If
+    End Sub
 
-    'Private Sub PlotView_Resize(sender As Object, e As EventArgs) Handles Me.Resize
-    '    Call Rendering()
-    'End Sub
+    Private Sub PlotView_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        Call Rendering()
+    End Sub
 End Class
