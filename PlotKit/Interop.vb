8b2caa3deb62a2b59062478133b9469573f365f2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-01 19:17:58 +0800

    make conversion

---
+++ PlotKit/Interop.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Imaging.Driver
+
+Module Interop
+
+    <Extension>
+    Public Function GetGdiPlusRasterImageResource(canvas As GdiRasterGraphics) As Image
+        Throw New NotImplementedException
+    End Function
+End Module
