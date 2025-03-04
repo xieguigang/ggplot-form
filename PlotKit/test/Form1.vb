1614790ecd2feb2108b16da5f57f4368b55f24b7
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-04 17:32:28 +0800

    make improvements of the dataframe operatoion

--- PlotKit/test/Form1.vb
+++ PlotKit/test/Form1.vb
@@ -38,14 +38,17 @@ Public Class Form1
 
     Private Sub pcaTest()
         Dim iris = DataFrame.read_csv("E:\GCModeller\src\R-sharp\REnv\data\bezdekIris.csv")
-        Dim classes As StringVector = iris!class
-        Call iris.delete("class")
+        Dim classes As StringVector = iris.delete("class")
         Dim pca = iris.CommonDataSet().PrincipalComponentAnalysis(maxPC:=2).GetPCAScore
-        Dim plot As ggplot.ggplot = ggplotFunction.ggplot(pca.add("class", classes), mapping:=aes("PC1", "PC2", color:="class"), colorSet:="jet", padding:="padding: 5% 10% 10% 10%;")
+        Dim plot As ggplot.ggplot = ggplotFunction.ggplot(
+            data:=pca.add("class", classes),
+            mapping:=aes("PC1", "PC2", color:="class"),
+            colorSet:="jet",
+            padding:="padding: 5% 10% 10% 10%;")
 
         plot += geom_point(size:=12)
 
-        view.ScaleFactor = 1
+        view.ScaleFactor = 1.25
         view.PlotPadding = plot.ggplotTheme.padding
         view.ggplot = plot
     End Sub
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ``Plot#``
-A interactive data visualization canvas for ggplot.
+
+A interactive data visualization canvas for ggplot on clr environment.
+
+![](./docs/Capture.PNG)
+
+```vbnet
+Dim iris = DataFrame.read_csv("./bezdekIris.csv")
+Dim classes As StringVector = iris!class
+Call iris.delete("class")
+Dim pca = iris.CommonDataSet().PrincipalComponentAnalysis(maxPC:=2).GetPCAScore
+Dim plot As ggplot.ggplot = ggplotFunction.ggplot(pca.add("class", classes), mapping:=aes("PC1", "PC2", color:="class"), colorSet:="jet", padding:="padding: 5% 10% 10% 10%;")
+
+plot += geom_point(size:=12)
+
+view.ScaleFactor = 1
+view.PlotPadding = plot.ggplotTheme.padding
+view.ggplot = plot
+```
