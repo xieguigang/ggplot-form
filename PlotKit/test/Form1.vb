f075f6ce6f74dfc35bcca66edc307389ee3a8763
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-04 17:20:04 +0800

    make improvements of add data field

--- PlotKit/test/Form1.vb
+++ PlotKit/test/Form1.vb
@@ -41,9 +41,9 @@ Public Class Form1
         Dim classes As StringVector = iris!class
         Call iris.delete("class")
         Dim pca = iris.CommonDataSet().PrincipalComponentAnalysis(maxPC:=2).GetPCAScore
-        Dim plot As ggplot.ggplot = ggplotFunction.ggplot(pca, mapping:=aes("PC1", "PC2"), colorSet:="jet", padding:="padding: 5% 10% 10% 10%;")
+        Dim plot As ggplot.ggplot = ggplotFunction.ggplot(pca.add("class", classes), mapping:=aes("PC1", "PC2", color:="class"), colorSet:="jet", padding:="padding: 5% 10% 10% 10%;")
 
-        plot += geom_point(size:=12, color:=classes.ToArray)
+        plot += geom_point(size:=12)
 
         view.PlotPadding = plot.ggplotTheme.padding
         view.ggplot = plot
