a47c09200bb80dfdb9db09fa25363bd0660ea773
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-04 17:13:31 +0800

    fix the bug of measure data size

--- PlotKit/test/Form1.vb
+++ PlotKit/test/Form1.vb
@@ -2,9 +2,11 @@
 Imports Microsoft.VisualBasic.Data.Framework
 Imports Microsoft.VisualBasic.Drawing
 Imports Microsoft.VisualBasic.Imaging.Driver
+Imports Microsoft.VisualBasic.Language.Vectorization
 Imports Microsoft.VisualBasic.Math
 Imports Microsoft.VisualBasic.Math.Distributions
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports Microsoft.VisualBasic.Math.Statistics.Hypothesis.ANOVA
 Imports Microsoft.VisualBasic.Math.VBMath
 Imports PlotKit
 
@@ -24,6 +26,30 @@ Public Class Form1
         Controls.Add(view)
         view.Dock = DockStyle.Fill
 
+        ' Call gaussTest()
+        Call pcaTest()
+    End Sub
+
+    Private Sub view_SizeChanged(sender As Object, e As EventArgs) Handles view.SizeChanged
+        If view.Debug Then
+            Me.Text = view.LastRenderCounter.ToString
+        End If
+    End Sub
+
+    Private Sub pcaTest()
+        Dim iris = DataFrame.read_csv("E:\GCModeller\src\R-sharp\REnv\data\bezdekIris.csv")
+        Dim classes As StringVector = iris!class
+        Call iris.delete("class")
+        Dim pca = iris.CommonDataSet().PrincipalComponentAnalysis(maxPC:=2).GetPCAScore
+        Dim plot As ggplot.ggplot = ggplotFunction.ggplot(pca, mapping:=aes("PC1", "PC2"), colorSet:="jet", padding:="padding: 5% 10% 10% 10%;")
+
+        plot += geom_point(size:=12, color:=classes.ToArray)
+
+        view.PlotPadding = plot.ggplotTheme.padding
+        view.ggplot = plot
+    End Sub
+
+    Private Sub gaussTest()
         Dim x As Double() = seq(0, 50, by:=0.05).ToArray
         Dim y1 = Gaussian.Gaussian(x, 12000, 15, 1)
         Dim y2 = Gaussian.Gaussian(x, 300, 27, 2)
@@ -39,14 +65,8 @@ Public Class Form1
 
         view.PlotPadding = plot.ggplotTheme.padding
         view.ggplot = plot
-        test.rownames = x.Select(Function(xi, i) CStr(i + 1)).ToArray
 
+        ' test.rownames = x.Select(Function(xi, i) CStr(i + 1)).ToArray
         '  Call test.WriteCsv("./test_signal.csv")
-    End Sub
-
-    Private Sub view_SizeChanged(sender As Object, e As EventArgs) Handles view.SizeChanged
-        If view.Debug Then
-            Me.Text = view.LastRenderCounter.ToString
-        End If
     End Sub
 End Class
